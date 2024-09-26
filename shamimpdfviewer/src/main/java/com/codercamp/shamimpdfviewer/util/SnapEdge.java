fa7a86d1db74cdb8a78965494781aa77cb112e48
mdshamimhossain <mdshamimh529@gmail.com>
2024-09-26 21:38:54 +0600

    Add Support Library

--- shamimpdfviewer/src/main/java/com/codercamp/shamimpdfviewer/util/SnapEdge.java
+++ shamimpdfviewer/src/main/java/com/codercamp/shamimpdfviewer/util/SnapEdge.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017 Bartosz Schiller
+ * Copyright 2024 Md. Shamim Hossain
  * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
