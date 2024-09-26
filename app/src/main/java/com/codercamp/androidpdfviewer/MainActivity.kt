c242c01754804d5d05c0a6119debc95b77bbe7a4
mdshamimhossain <mdshamimh529@gmail.com>
2024-09-26 22:34:34 +0600

    Add Support Library

--- app/src/main/java/com/codercamp/androidpdfviewer/MainActivity.kt
+++ app/src/main/java/com/codercamp/androidpdfviewer/MainActivity.kt
@@ -1,15 +1,76 @@
 package com.codercamp.androidpdfviewer
 
+import android.os.AsyncTask
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.codercamp.shamimpdfviewer.PDFView
+import com.codercamp.shamimpdfviewer.link.DefaultLinkHandler
+import com.codercamp.shamimpdfviewer.util.FitPolicy
+import java.io.BufferedInputStream
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var pdfView: PDFView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val pdfUrl = "https://unec.edu.az/application/uploads/2014/12/pdf-sample.pdf"
 
+        pdfView = findViewById(R.id.pdfView)
+        RetrievePDFFromURL(pdfView).execute(pdfUrl)
+    }
+
+    class RetrievePDFFromURL(pdfView: PDFView) :
+        AsyncTask<String, Void, InputStream>() {
+
+        // on below line we are creating a variable for our pdf view.
+        val mypdfView: PDFView = pdfView
+
+        // on below line we are calling our do in background method.
+        override fun doInBackground(vararg params: String?): InputStream? {
+            // on below line we are creating a variable for our input stream.
+            var inputStream: InputStream? = null
+            try {
+                // on below line we are creating an url
+                // for our url which we are passing as a string.
+                val url = URL(params.get(0))
+
+                // on below line we are creating our http url connection.
+                val urlConnection: HttpURLConnection = url.openConnection() as HttpsURLConnection
+
+                // on below line we are checking if the response
+                // is successful with the help of response code
+                // 200 response code means response is successful
+                if (urlConnection.responseCode == 200) {
+                    // on below line we are initializing our input stream
+                    // if the response is successful.
+                    inputStream = BufferedInputStream(urlConnection.inputStream)
+                }
+            }
+            // on below line we are adding catch block to handle exception
+            catch (e: Exception) {
+                // on below line we are simply printing
+                // our exception and returning null
+                e.printStackTrace()
+                return null;
+            }
+            // on below line we are returning input stream.
+            return inputStream;
+        }
+
+        // on below line we are calling on post execute
+        // method to load the url in our pdf view.
+        override fun onPostExecute(result: InputStream?) {
+            // on below line we are loading url within our
+            // pdf view on below line using input stream.
+            mypdfView.fromStream(result).load()
+
+        }
     }
 }
